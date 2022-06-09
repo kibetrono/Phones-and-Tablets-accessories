10297178cdd0ed9ed5e2a0c7df66fa4696c62d7f
kibetrono <kibetdavidro@gmail.com>
2022-06-09 15:33:32 +0300

    create customerreturns view files

---
+++ resources/views/customerreturns/create.blade.php
@@ -0,0 +1,125 @@
+<form method="POST" action="{{ route('customerreturns.store') }}">
+                        @csrf
+<div class="modal-body">
+    <div class="row">        
+
+        <div class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('serial_number', __('Serial Number'),['class'=>'form-label']) }}<span class="text-danger">*</span>
+                <div class="form-icon-user">
+                  {{ Form::select('serial_number', array_merge(['' => "Select Serial Number"] + $product_serial_number),null, array('class' => 'form-control select2','id'=>'choices-multiple1','required'=>'required')) }}
+
+                </div>
+            </div>
+        </div>
+
+         <div class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('model_name', __('Model Name'),['class'=>'form-label']) }}<span class="text-danger">*</span>
+                <div class="form-icon-user">
+                   <input name="model_name" id="model_name_id" class="form-control" type="text" required readonly>              
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('imei_number', __('IMEI Number'),['class'=>'form-label']) }}<span class="text-danger">*</span>
+                <div class="form-icon-user">
+                   <input name="imei_number" id="imei_number_id" class="form-control" type="number" required readonly>              
+                </div>
+            </div>
+        </div>
+
+        <div style="display:none" class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('quantity_delivered', __('Quantity Delivered'),['class'=>'form-label']) }}<span class="text-danger">*</span>
+                <div class="form-icon-user">
+                   <input name="quantity_delivered" value="1" class="form-control" type="hidden" required readonly>              
+                </div>
+            </div>
+        </div>
+
+         <div class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('invoice_number', __('Invoice Number'),['class'=>'form-label']) }}
+                <div class="form-icon-user">
+                   <input name="invoice_number" id="invoice_number_id"  class="form-control" type="number" required readonly>              
+                </div>
+            </div>
+        </div>
+
+        
+                {{-- start of customers name --}}
+        <div class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('returning_customer', __('Customers Name'),['class'=>'form-label']) }}
+                <div class="form-icon-user">
+                   {{ Form::select('returning_customer', array_merge(['' => "Select Customer"] +$returning_customer),null, array('class' => 'form-control select2','id'=>'choices-multiple2','required'=>'required')) }}
+
+                </div>
+            </div>
+        </div>
+        {{-- end of customers name--}}
+
+        {{-- start of person receiving --}}
+        <div style="display:none" class="col-md-6">
+            <div class="form-group">
+                {{ Form::label('receiving_person', __('Receiving Person'),['class'=>'form-label']) }}
+                <div class="form-icon-user">
+                    {{ Form::hidden('receiving_person', $receiving_person,null, array('class' => 'form-control','required'=>'required')) }}
+                </div>
+            </div>
+        </div>
+        {{-- end of person receiving--}}
+
+        @if(!$customFields->isEmpty())
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="tab-pane fade show" id="tab-2" role="tabpanel">
+                    @include('customFields.formBuilder')
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
+<div class="modal-footer">
+    <input type="button" value="{{__('Cancel')}}" class="btn  btn-light" data-bs-dismiss="modal">
+    <input type="submit" value="{{__('Submit')}}" class="btn  btn-primary">
+</div>
+</form>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+
+<script>
+
+ $(document).ready(function(){
+    $(document).on('change','#choices-multiple1',function(){
+        let prod_name = $(this).val();
+        $.ajax({ 
+            url:'getmodelname',type: 'get',dataType: 'json',
+            data:{'serial_number':prod_name},
+            success: function(response) {if (response != null) {$('#model_name_id').val(response.model_name);}},error:function(){ }
+        });      
+    })
+
+     $(document).on('change','#choices-multiple1',function(){
+        let prod_name = $(this).val();
+        $.ajax({ 
+            url:'getimeinumber',type: 'get',dataType: 'json',
+            data:{'serial_number':prod_name},
+            success: function(response) {if (response != null) {$('#imei_number_id').val(response.imei_number);}},error:function(){ }
+        });      
+    })
+
+    $(document).on('change','#choices-multiple1',function(){
+        let prod_name = $(this).val();
+        $.ajax({ 
+            url:'getinvoicenumber',type: 'get',dataType: 'json',
+            data:{'serial_number':prod_name},
+            success: function(response) {if (response != null) {$('#invoice_number_id').val(response.invoice_number);}},error:function(){ }
+        });      
+    })
+
+});
+   
+</script> 
---
+++ resources/views/customerreturns/index.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.admin')
+@php
+    $profile=asset(Storage::url('uploads/avatar/'));
+@endphp
+@push('script-page')
+    <script>
+        </script>
+
+@endpush
+@section('page-title')
+    {{__('Manage Customer Returns')}}
+@endsection
+@push('script-page')
+@endpush
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="{{route('dashboard')}}">{{__('Dashboard')}}</a></li>
+    <li class="breadcrumb-item">{{__('Customer Returns')}}</li>
+@endsection
+@section('action-btn')
+    <div class="float-end">
+        <a href="#" data-size="md"  data-bs-toggle="tooltip" title="{{__('Import')}}" data-url="{{ route('customerreturns.file.import') }}" data-ajax-popup="true" data-title="{{__('Import Customer Returns CSV file')}}" class="btn btn-sm btn-primary">
+            <i class="ti ti-file-import"></i>
+        </a>
+        <a href="{{route('customerreturns.export')}}" data-bs-toggle="tooltip" title="{{__('Export')}}" class="btn btn-sm btn-primary">
+            <i class="ti ti-file-export"></i>
+        </a>
+
+       <a href="#" data-size="lg" data-url="{{ route('customerreturns.create') }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create')}}" data-title="{{__('Create Customer Returns')}}" class="btn btn-sm btn-primary">
+            <i class="ti ti-plus"></i>
+        </a>
+
+    </div>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-body table-border-style">
+                    <h5></h5>
+                    <div class="table-responsive">
+                        <table class="table datatable">
+                            <thead>
+                                <tr>
+                                    
+                                    <th> {{__('Product Name')}}</th>
+                                    <th> {{__('IMEI NO.')}}</th>
+                                    <th> {{__('SERIAL NO.')}}</th>
+                                    <th> {{__('INVOICE NO.')}}</th>
+                                    <th> {{__('CUSTOMERS NAME')}}</th>
+                                    <th>{{__('Action')}}</th>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                           
+                                @foreach ($customerReturns as $k=>$cust_returns)
+                                <tr class="custreturns_tr" id="custreturns_detail" data-url="{{route('customerreturns.show',$cust_returns['id'])}}" data-id="{{$cust_returns['id']}}">
+                                    
+                                    <td class="font-style">{{$cust_returns['model_name']}}</td>
+                                    <td>{{$cust_returns['imei_number']}}</td>
+                                    <td>{{$cust_returns['serial_number']}}</td>
+
+                                    <td>{{$cust_returns['invoice_number']}}</td>
+                                    <td>{{$cust_returns['returning_customer']}}</td>
+                                   
+                                    <td class="Action">
+                                        <span>
+                                        @if($cust_returns['is_active']==0)
+                                                <i class="ti ti-lock" title="Inactive"></i>
+                                            @else
+
+                                                <div class="action-btn bg-warning ms-2">
+                                                    <a href="#" class="mx-3 btn btn-sm  align-items-center" data-url="{{route('customerreturns.reset',\Crypt::encrypt($cust_returns['id']))}}" data-ajax-popup="true"  data-size="md" data-bs-toggle="tooltip" title="{{__('Forgot Password')}}"  data-title="{{__('Reset Password')}}">
+                                                        <i class="ti ti-key text-white"></i>
+                                                    </a>
+                                                </div>
+                                                @can('show customer')
+                                                <div class="action-btn bg-info ms-2">
+                                                    <a href="{{ route('customerreturns.show',\Crypt::encrypt($cust_returns['id'])) }}" class="mx-3 btn btn-sm align-items-center"
+                                                       data-bs-toggle="tooltip" title="{{__('View')}}">
+                                                        <i class="ti ti-eye text-white text-white"></i>
+                                                    </a>
+                                                </div>
+                                                @endcan
+                                                @can('edit customer')
+                                                    <div class="action-btn bg-primary ms-2">
+                                                        <a href="#" class="mx-3 btn btn-sm  align-items-center" data-url="{{ route('customerreturns.edit',$cust_returns['id']) }}" data-ajax-popup="true"  data-size="xl" data-bs-toggle="tooltip" title="{{__('Edit')}}"  data-title="{{__('Edit Delivery Personel\'s Details')}}">
+                                                            <i class="ti ti-pencil text-white"></i>
+                                                        </a>
+                                                    </div>
+
+                                                @endcan
+
+
+
+                                                @can('delete customer')
+                                                    <div class="action-btn bg-danger ms-2">
+                                                        {!! Form::open(['method' => 'DELETE', 'route' => ['customerreturns.destroy', $cust_returns['id']],'id'=>'delete-form-'.$cust_returns['id']]) !!}
+                                                        <a href="#" class="mx-3 btn btn-sm  align-items-center bs-pass-para" data-bs-toggle="tooltip" title="{{__('Delete')}}" ><i class="ti ti-trash text-white text-white"></i></a>
+                                                        {!! Form::close() !!}
+                                                    </div>
+                                                @endcan
+
+                                            @endif
+                                        </span>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
